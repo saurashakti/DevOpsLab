bb120c193f9173ba737951c9ae4d94d367f7942e
saurashakti <saurashakti@gmail.com>
2022-01-21 10:48:57 +0530

    script updated

--- Ansible/Read-eventlogsParams.ps1
+++ Ansible/Read-eventlogsParams.ps1
@@ -8,5 +8,6 @@ param(
 
 $ParameterName
 #$slog = Get-WinEvent -LogName "Microsoft-Windows-PowerShell/Operational" -MaxEvents 10 | select-object TimeCreated, Message | Where-Object {$_.message -like "*console*"} |format-table -AutoSize | Out-String
+Write-host "Calling parameters $logName $searchstring and $maxevents"
 $slog = Get-WinEvent -LogName $LogName -MaxEvents $MaxEvents | select-object TimeCreated, Message | Where-Object {$_.message -like "*$searchstring*"} |format-table -AutoSize | Out-String
 write-host $slog
